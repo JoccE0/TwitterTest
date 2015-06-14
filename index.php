c23498fae39661d0369dcb56c05dbeefe9b1b13d
Pajkanon <Jocce@jocce.nu>
2015-06-14 19:54:05 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+require('Twitter.php');
+require('MulticraftAPI.php');
+
+$api = new MulticraftAPI('http://example.com/api.php', 'USERNAME', 'API-CODE');
+$twtr = new Twitter;
+
+$serverid = 1;
+//$data = $api->getServerStatus($serverid, true);
+$data = $api->getServerStatus($serverid, true)['data']['status'];
+
+echo $data;
+
+if ($data = "online") {
+	$twtr->tweet('Server is Online');
+}else {
+	$twtr->tweet('Server is offline');
+}	
+
+?>
